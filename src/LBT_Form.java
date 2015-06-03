ca3ba79a56902af2003cec370a1f276417a20da6
Tibor Kertesz <hummerdikk@gmail.com>
2015-06-03 11:31:26 +0200

    Modified names
    by Tibi

---
+++ src/LBT_Form.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+/**
+ * Created by okocsis
+ * Modified by hummerdikk
+ */
+public class LBT_Form extends JFrame {
+    private JButton generateButton;
+    public JPanel LBT_Panel;
+
+    public LBT_Form() {
+        super("LBT_Form");
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
-        MyForm frame = new MyForm();
-        frame.setContentPane(new MyForm().panel1);
+        LBT_Form frame = new LBT_Form();
+        frame.setContentPane(new LBT_Form().LBT_Panel);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
